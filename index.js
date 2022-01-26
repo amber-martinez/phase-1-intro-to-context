a7dd1249e30c68adf5233bbf08f0128dc5253813
Amber <martinez.amber1028@gmail.com>
2022-01-25 16:43:12 -0800

    Pushing for help

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-//createEmployeeRecord returns regEmp, an object
 function createEmployeeRecord(empInfo) {
 
     let regEmp = Object.assign({}, empInfo)
-//return an array of all these created
         regEmp.firstName = empInfo[0];
         regEmp.familyName = empInfo[1];
         regEmp.title = empInfo[2];
@@ -17,38 +15,42 @@ function createEmployeeRecord(empInfo) {
 function createEmployeeRecords(arrayOfArrays) {
     let newArray = [];
     for (let array of arrayOfArrays) {
-//         console.log(createEmployeeRecord(array));
-        //only returning the first if return is here
         newArray.push(createEmployeeRecord(array))
     }
     return newArray;
 }
 
-// let empRec = createEmployeeRecord(['Amber', 'Martinez', 'Engineer', 1000])
-
 function createTimeInEvent(empRec, dateStamp) {
-
-    empRec.timeInEvents.timeInObj = {}
-
-    // let type = 'TimeIn';
-    empRec.timeInEvents.timeInObj.type = 'TimeIn';
-
-    // let hour = dateStamp.slice(11, 15);
-    empRec.timeInEvents.timeInObj.hour = dateStamp.slice(11, 15);
-
-    // let date = dateStamp.slice(0, 10);
-    empRec.timeInEvents.timeInObj.date = dateStamp.slice(0, 10);
-
-    // return empRec.timeInEvents.timeInObj;
-    return empRec.timeInEvents
+    let timeInObj = {}
+    timeInObj.type = 'TimeIn';
+    timeInObj.hour = parseInt(dateStamp.slice(11, 15));
+    timeInObj.date = dateStamp.slice(0, 10);
+    empRec.timeInEvents.push(timeInObj);
+    return empRec
 }
 
-// "YYYY-MM-DD HHMM"
-// "2022-01-22"
-    //ex. empRec = createEmployeeRecords()
-// Argument(s) - An employee record Object, A date stamp ("YYYY-MM-DD HHMM")
-// Returns- The employee record
-// Behavior - Add an Object with keys to the timeInEvents Array on the record Object:
-//            type: Set to "TimeIn"
-//            hour: Derived from the argument
-//            date: Derived from the argument
+function createTimeOutEvent(empRec, dateStamp) {
+    let timeOutObj = {}
+    timeOutObj.type = 'TimeOut';
+    timeOutObj.hour = parseInt(dateStamp.slice(11, 15));
+    timeOutObj.date = dateStamp.slice(0, 10);
+    empRec.timeOutEvents.push(timeOutObj)
+
+    return empRec
+}
+
+function hoursWorkedOnDate(empRec, date) {
+
+    let timeIn = createTimeInEvent(empRec, date);
+    let timeOut = createTimeOutEvent(empRec, date);
+
+    return timeOut.timeOutEvents.timeOutObj.hour - timeIn.timeInEvents.timeInObj.hour
+//reduce method?
+}
+
+
+
+
+// Argument(s) - An employee record Object, A date of the form - "YYYY-MM-DD"
+// Returns - Hours worked, an Integer
+// Behavior - Given a date, find the number of hours elapsed between that date's timeInEvent and timeOutEvent
